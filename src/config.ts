6a22370c70eba866f917c9e5435b73e1433afc02
aotle <1781322024@qq.com>
2024-07-18 23:41:31 +0800

    fix footer

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Yakamoz',
-  subtitle: 'Aotle',
-  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  subtitle: 'Open your mind to the possibilities.',
+  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
@@ -22,9 +22,9 @@ export const siteConfig: SiteConfig = {
   },
   favicon: [    // Leave this array empty to use the default favicon
     {
-      src: 'https://cdn.jsdelivr.net/gh/xaoxuu/assets@master/favicon/favicon.ico',    // Path of the favicon, relative to the /public directory
-      theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-      sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+      src: 'assets/images/favicon.ico',    // Path of the favicon, relative to the /public directory
+      //theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      //sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     }
   ]
 }
